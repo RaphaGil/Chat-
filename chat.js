abf960ab4ffeaa8eeb1a474f591240de33dce89d
Raphaela <128820385+RaphaGil@users.noreply.github.com>
2023-05-16 09:32:20 +0100

    Add files via upload

--- chat.js
+++ chat.js
@@ -1,127 +1,91 @@
-// Puttinc action to select bottom and show up the box
 const chatIcon = document.getElementById('chat_icon');
+const chatInputAllBox = document.getElementById('input')
+
 const chatBoxContainer = document.getElementById('chat-box-container');
 
-
-chatIcon.addEventListener('click', ()=> {
-    chatBoxContainer.style.display = chatBoxContainer.style.display = 'block';
-  });
-
-
-//Pop up the message with the customer msg and options:
-
-    const customerMensagem = document.getElementById('send-button');
-    customerMensagem.addEventListener('click', function(e) {
-      e.preventDefault();
-      const mensagemRecebida = document.getElementById('chat-input').value;
-      const resultadoContainer = document.getElementById('received-msg-inbox');
-      resultadoContainer.innerHTML = `${mensagemRecebida}`;
-      resultadoContainer.style.display = 'grid';
-
-      const nextMessage = document.getElementById('primeirapergunta');
-      nextMessage.style.display='grid'
-
-    });
-
-//Select the message and pop Up a reply and another option
-
-
-//
-const customerSelectSales = document.getElementsByClassName('btn')[0];
-const customerSelectSalesSecond = document.getElementById('segundaPerguntaSales');
-customerSelectSales.addEventListener('click', function(e){
-  e.preventDefault();
-  customerSelectSalesSecond.style.display = 'grid';
-  chatBoxContainer.scrollTop = chatBoxContainer.scrollHeight;
-
+chatIcon.addEventListener('click', () => {
+  chatBoxContainer.classList.toggle('show');
+chatInputAllBox.classList.toggle('show');
 });
 
-const customerSelectSupport = document.getElementsByClassName('btn')[1];
-const customerSelectSecond = document.getElementById('segundaPerguntaSupport');
-customerSelectSupport.addEventListener('click', function(e){
+const button = document.getElementById('send-button');
+
+button.addEventListener('click', (e) => {
   e.preventDefault();
-  customerSelectSecond.style.display = 'grid';
-  chatBoxContainer.scrollTop = chatBoxContainer.scrollHeight;
-})
-// Support answers:
- const WarrantyClaim = document.getElementsByClassName('btn1')[0];
- const warrantyClaimContainer = document.getElementById('warrantyClaim');
- const resultadoClaim = document.getElementById('received-msg-inbox-warrantyclaim');
- WarrantyClaim.addEventListener('click', function(e){
-     e.preventDefault();
-     resultadoClaim.innerHTML = WarrantyClaim.textContent;
-   
-     warrantyClaimContainer.style.display = 'grid';
-     chatBoxContainer.scrollTop = chatBoxContainer.scrollHeight;
-  })
- 
-  const SparePart= document.getElementsByClassName('btn1')[1];
-  const SparePartContainer = document.getElementById('SpareParts');
-  const resultadoSpareParts = document.getElementById('received-msg-inbox-spareparts');
-  SparePart.addEventListener('click', function(e){
-      e.preventDefault();
-      resultadoSpareParts.innerHTML = SparePart.textContent;
-      resultadoSpareParts.style.display = 'grid';
-      resultadoSpareParts.insertAdjacentElement('afterend', SparePartContainer);
-      SparePartContainer.style.display = 'grid';
-      chatBoxContainer.scrollTop = chatBoxContainer.scrollHeight;
-   })
-  
-   const TechnicalSupport= document.getElementsByClassName('btn1')[2];
-   const TechnicalSupportContainer = document.getElementById('technicalSupport');
-   const resultadoTechnicalSupport = document.getElementById('received-msg-inbox-technicalsupport');
-   
-   TechnicalSupport.addEventListener('click', function(e){
-       e.preventDefault();
 
-       resultadoTechnicalSupport.innerHTML = TechnicalSupport.textContent;
-       resultadoTechnicalSupport.style.display = 'grid';
-       resultadoTechnicalSupport.insertAdjacentElement('afterend', TechnicalSupportContainer);
-       TechnicalSupportContainer.style.display = 'grid';
-       chatBoxContainer.scrollTop = chatBoxContainer.scrollHeight;
-    })  
+  const chatInput = document.getElementById('chat-input').value;
+  const receivedMsgInbox = document.getElementById('received-msg-inbox');
+  const primeiraPergunta = document.getElementById('primeirapergunta');
+
+  receivedMsgInbox.innerHTML = chatInput;
+  receivedMsgInbox.style.display = 'grid';
+  primeiraPergunta.style.display = 'grid';
+  chatInput.insertAdjacentElement('afterend', chatBoxContainer);
+
+  scrollBottom();
+});
+
+const buttons = document.querySelectorAll('.btn');
+
+for (let i = 0; i < buttons.length; i++) {
+  const buttonChat = buttons[i];
+  buttonChat.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    switch (buttonChat) {
+      case buttons[0]:
+        const priceBoxContainer = document.getElementById('segundaPerguntaSales');
+        priceBoxContainer.style.display = 'grid';
+
     
-    const SpreedSheet= document.getElementsByClassName('btn1')[3];
-    const SpreedSheetContainer = document.getElementById('spreedSheet');
-    const resultadoSpreedSheet = document.getElementById('received-msg-inbox-spreedsheets');
-    SpreedSheet.addEventListener('click', function(e){
-        e.preventDefault();
-        resultadoSpreedSheet .innerHTML = SpreedSheet.textContent;
-        resultadoSpreedSheet .style.display = 'grid';
-        resultadoSpreedSheet.insertAdjacentElement('afterend', SpreedSheetContainer);
-        SpreedSheetContainer.style.display = 'grid';
-        chatBoxContainer.scrollTop = chatBoxContainer.scrollHeight;
-     })
+      
+        break;
 
-     //Sales Answers:
+      case buttons[1]:
+        const customerSelectSecond = document.getElementById('segundaPerguntaSupport');
+        customerSelectSecond.style.display = 'grid';
+ 
 
-     const priceCheck = document.getElementsByClassName('btn1')[4];
-     const priceBoxContainer = document.getElementById('priceinfo');
-     const resultadoPrice = document.getElementById('received-msg-inbox-price');
-     //const customerSelectSalesSecond = document.getElementById('segundaPerguntaSales');
-     priceCheck.addEventListener('click', function(e){
-       e.preventDefault();
+        break;
+
+      case buttons[2]:
+        const supportSelectSecond = document.getElementById('warrantyClaim');
+        supportSelectSecond.style.display = 'grid';
+
+    
+        break;
+
+      case buttons[3]:
+        const supportSelectThird = document.getElementById('SpareParts');
+        supportSelectThird.style.display = 'grid';
      
-       priceBoxContainer.style.display = 'grid';
-       resultadoPrice.innerHTML = priceCheck.textContent;
-       resultadoPrice.style.display = 'grid';
-      resultadoPrice.insertAdjacentElement('afterend', priceBoxContainer);
-     
-       chatBoxContainer.scrollTop = chatBoxContainer.scrollHeight;
-     });
 
-      const Delivery = document.getElementsByClassName('btn1')[5];
-      const DeliveryContainer = document.getElementById('delivery');
-      Delivery.addEventListener('click', function(e){
-          e.preventDefault();
-          DeliveryContainer.style.display = 'grid';
-          chatBoxContainer.scrollTop = chatBoxContainer.scrollHeight;
-       })
-       const Question= document.getElementsByClassName('btn1')[6];
-       const questionContainer = document.getElementById('question');
-       Question.addEventListener('click', function(e){
-           e.preventDefault();
-           questionContainer.style.display = 'grid';
-           chatBoxContainer.scrollTop = chatBoxContainer.scrollHeight;
-        })
-        
+        break;
+
+      case buttons[4]:
+        const supportSelectFourth = document.getElementById('technicalSupport');
+        supportSelectFourth.style.display = 'grid';
+  
+
+        break;
+
+      case buttons[5]:
+        const supportSelectFifth = document.getElementById('spreedSheet');
+        supportSelectFifth.style.display = 'grid';
+
+
+        break;
+
+      default:
+        break;
+    }
+
+    scrollBottom();
+  });
+}
+
+
+
+function scrollBottom() {
+  chatBoxContainer.scrollTo(0, chatBoxContainer.scrollHeight);
+}
